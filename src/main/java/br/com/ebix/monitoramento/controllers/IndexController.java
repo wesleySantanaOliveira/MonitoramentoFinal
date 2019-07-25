b05811476fd83c52dd3c3d3f7502b58e480db97d
Wesley Oliveira <wesley.santana.oliveira@outlook.com>
2019-07-25 10:41:55 -0300

    Ateracoes dois

--- src/main/java/br/com/ebix/monitoramento/controllers/IndexController.java
+++ src/main/java/br/com/ebix/monitoramento/controllers/IndexController.java
@@ -1,8 +1,15 @@
 package br.com.ebix.monitoramento.controllers;
 
-/*
- * @Controller public class IndexController {
- * 
- * @RequestMapping(value="/", method=RequestMethod.GET) public String index() {
- * return "redirect:/index.xhtml"; } }
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+  public class IndexController {
+  
+  @RequestMapping(value="/", method=RequestMethod.GET) 
+  public String index() {
+	  return "redirect:/index.xhtml";
+	  }
+  }
+ 
