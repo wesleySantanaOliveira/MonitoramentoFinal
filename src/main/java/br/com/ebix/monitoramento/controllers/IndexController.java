171f11a30ecf905a22fa61b6b1386226151f5469
Wesley Oliveira <wesley.santana.oliveira@outlook.com>
2019-07-25 10:38:04 -0300

    Atualizado

--- src/main/java/br/com/ebix/monitoramento/controllers/IndexController.java
+++ src/main/java/br/com/ebix/monitoramento/controllers/IndexController.java
@@ -1,14 +1,8 @@
 package br.com.ebix.monitoramento.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-@Controller
-public class IndexController {
-	
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String index() {
-		return "redirect:/index.xhtml";
-	}	
-}
+/*
+ * @Controller public class IndexController {
+ * 
+ * @RequestMapping(value="/", method=RequestMethod.GET) public String index() {
+ * return "redirect:/index.xhtml"; } }
+ */
