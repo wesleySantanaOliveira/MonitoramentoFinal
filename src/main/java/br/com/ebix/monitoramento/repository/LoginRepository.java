f23761b03f170b8682fb6ec9188fcd152b5683b2
wesleysantana <wesleysantana@NBBV030202.BRQ.COM>
2019-10-15 13:25:25 -0300

    Criado de APIs. de Eventos.

--- src/main/java/br/com/ebix/monitoramento/repository/LoginRepository.java
+++ src/main/java/br/com/ebix/monitoramento/repository/LoginRepository.java
@@ -1,30 +1,18 @@
 package br.com.ebix.monitoramento.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
+import br.com.ebix.monitoramento.model.Evento;
 import br.com.ebix.monitoramento.model.Usuario;
 
-@Repository
-public interface LoginRepository extends JpaRepository<Usuario, Long> {
+public interface LoginRepository extends JpaRepository<Usuario, Long>{
 	
-	  @Query("select u from Usuario u where u.email=:email") 
-	  public Usuario buscarPorEmail(@Param("email") String email);
-	  
-	  @Query("select u from Usuario u where u.pass=:pass")
-	  public Usuario buscarPorSenha(@Param("pass") String pass);
-	  
-	  @Query("select u from Usuario u ORDER BY u.idUser DESC") 
-	  public List<Usuario> buscarTodos();
-	  
-	  @Query("select u from Usuario u where u.nmUsuario=:nmUsuario")
-	  public Usuario buscarPorUsuario(@Param("nmUsuario") Usuario nmUsuario);
-	  
-	  @Query("select u from Usuario u") 
-	  public List<Usuario> listAll();	 
+	@Query("select u from Usuario u where u.email=:email")
+	public Evento buscarPorEmail(@Param("email") String email);
+	
+	@Query("select u from Usuario u where u.pass=:pass")
+	public Evento buscarPorPass(@Param("pass") String pass);
 
 }
